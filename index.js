40e7b81fdec6bea86f91d2e08f563d440ead535a
kevin <kevinalvapearson777@gmail.com>
2020-09-19 03:03:17 -0500

    creando el servidor de express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(' index.js')
+const express = require('express');
+
+// crear el servidor
+const app = express();
+
+// puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// Definir la pagina principal
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+})
+
+// arrancar la app
+app.listen(PORT, () => {
+    console.log(`el servidor está funcionando en el puerto ${PORT}`)
+});
