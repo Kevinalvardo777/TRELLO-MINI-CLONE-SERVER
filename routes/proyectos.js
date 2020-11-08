0e698bd7e563ba74776997e9563832e33c31df7c
kevin <kevinalvapearson777@gmail.com>
2020-11-08 17:54:30 -0500

    crud de proyectos

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const proyectoController = require('./../controllers/ProyectoController');
+const auth = require('./../middleware/auth');
+const { check } = require('express-validator');
 
 //Crea proyectos
 // api/proyectos
-router.post('/',
+router.post('/', 
+    auth,
+    [
+        check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+    ],
     proyectoController.crearProyecto
-)
+);
+
+//Obtener todos los proyectos
+router.get('/', 
+    auth,
+    proyectoController.obtenerProyectos
+);
+
+// Actualizar proyecto via Id
+router.put('/:id', 
+    auth, 
+    [
+        check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+    ],
+    proyectoController.actualizarProyecto
+);
+
+// Eliminar un proyecto
+router.delete('/:id',
+    auth,
+    proyectoController.eliminarProyecto
+);
 
 module.exports = router;
